902c28caae7b26cfca684e24d1bc4f4b49b74b30
Art-Araujo <artur.araujo.silva649@gmail.com>
2025-02-25 16:31:50 -0300

    Aula de PHP com exemplos de concatenação de variaveis

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pwii--artur-silva
 Aulas de Programação Web II com os professores João Siles e Ricardo Palhares
+Me chamo artur, tenho 16 anos e curto RPG (sou mestre) e pokemon
---
+++ aula-1/ola.php
@@ -0,0 +1,10 @@
+       <?php
+            // Comentario aewwwwwwwww portugol é rui
+            $Nome = "Artur";
+            $Numero = 69;
+            $Sobrenome = "Araujo Silva";
+
+            echo "${Nome} <h1> ${Sobrenome} </h1>" ;
+            echo $Numero . " <p> " . $Nome . " " . $Sobrenome . "</p> ";
+
+        ?>
