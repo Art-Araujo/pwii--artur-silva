137e72f70927c54b43086a47cbd132bc8ddfb5f2
Art-Araujo <artur.araujo.silva649@gmail.com>
2025-04-08 16:26:02 -0300

    aula 3 com o joão siles

---
+++ aula 3/decisao.php
@@ -0,0 +1,12 @@
+<?php 
+$x = 10;
+$y = 3;
+$media = $x + $y / 2
+
+if ($media >= 6.0){
+    echo'aluno aprovado';
+} else if ($media >= 4.0){
+    echo "aluno em recuperação";
+} else {
+    echo 'aluno reprovado';
+}
